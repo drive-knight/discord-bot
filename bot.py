946d13fca695edb430f978854071e457dbc4e207
Ivan Novikov <johanxvcii@gmail.com>
2021-06-01 16:52:56 +0500

    Commit

--- bot.py
+++ bot.py
@@ -5,30 +5,30 @@ import os
 settings = {
     'bot': 'info',
     'id': 843794727824523265,
-    'prefix': '!'
+    'prefix': '!',
 }
-
 
 bot = commands.Bot(command_prefix = settings['prefix'])
 
 @bot.command(pass_context=True)
 async def info(ctx, member: discord.Member = None):
-    if not member:  # if member is no mentioned
-        member = ctx.message.author  # set member as the author
+    if not member:
+        member = ctx.message.author
     roles = [role for role in member.roles]
-    embed = discord.Embed(colour=discord.Colour.purple(), timestamp=ctx.message.created_at,
+    emb = discord.Embed(colour=discord.Colour.purple(), timestamp=ctx.message.created_at,
                           title=f"User Info - {member}")
-    embed.set_thumbnail(url=member.avatar_url)
-    embed.set_footer(text=f"Requested by {ctx.author}")
+    emb.set_thumbnail(url=member.avatar_url)
+    emb.set_footer(text=f"Requested by {ctx.author}")
 
-    embed.add_field(name="ID:", value=member.id)
-    embed.add_field(name="Name:", value=member.display_name)
+    emb.add_field(name="ID:", value=member.id)
+    emb.add_field(name="Name:", value=member.display_name)
+    emb.add_field(name="Nick:", value=member.nick)
 
-    embed.add_field(name="Created Account On:", value=member.created_at.strftime("%d.%m.%Y, %I:%M %p UTC"))
-    embed.add_field(name="Joined Server On:", value=member.joined_at.strftime("%d.%m.%Y, %I:%M %p UTC"))
+    emb.add_field(name="Created Account On:", value=member.created_at.strftime("%d.%m.%Y, %I:%M %p UTC"))
+    emb.add_field(name="Joined Server On:", value=member.joined_at.strftime("%d.%m.%Y, %I:%M %p UTC"))
 
-    embed.add_field(name="Roles:", value="".join([role.mention for role in roles[1:]]))
-    await ctx.send(embed=embed)
+    emb.add_field(name="Roles:", value="".join([role.mention for role in roles[1:]]))
+    await ctx.send(embed=emb)
     
 token = os.environ.get('BOT_TOKEN')
 
