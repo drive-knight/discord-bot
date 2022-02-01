3f68be59fc923cdb4e71a692f9e3e5810464ab45
drive-knight <yavatar15@yandex.ru>
2022-02-01 05:00:58 +0500

    added a new command

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import random
 
 settings = {
     'bot': 'info',
@@ -9,6 +10,12 @@ settings = {
 }
 
 bot = commands.Bot(command_prefix=settings['prefix'])
+
+gifs = ('https://i.imgur.com/8JGpqi8.gif', 'https://i.imgur.com/EZrsYyu.gif', 'https://i.imgur.com/7zEEI0C.gif',
+        'https://i.imgur.com/0SpBiAn.gif', 'https://i.imgur.com/5mAm5yA.gif', 'https://i.imgur.com/xmdhGdV.gif',
+        'https://i.imgur.com/RV5YOMy.gif', 'https://i.imgur.com/BGcWxa0.gif', 'https://i.imgur.com/I8iN2iv.gif',
+        'https://i.imgur.com/40lsJPi.gif', 'https://i.imgur.com/FC73FLS.gif', 'https://i.imgur.com/RvdRd4O.gif'
+        'https://i.imgur.com/wQVdG5P.gif', 'https://i.imgur.com/p2lnrna.gif', 'https://i.imgur.com/wQVdG5P.gif')
 
 
 @bot.command(pass_context=True)
@@ -31,6 +38,12 @@ async def info(ctx, member: discord.Member = None):
     emb.add_field(name="Roles:", value="".join([role.mention for role in roles[1:]]))
     await ctx.send(embed=emb)
 
+
+@bot.command()
+async def ts(ctx):
+    embed = discord.Embed()
+    embed.set_image(url=random.choice(gifs))
+    await ctx.send(embed=embed)
 
 token = os.environ.get('BOT_TOKEN')
 
